64029e7d4d1ba6a60f101fe47b4456c2fa8a5920
steve-mir <ekechukwuemeka25@gmail.com>
2023-01-18 14:36:50 +0100

    Implementing tests

---
+++ tests/user.test.js
@@ -0,0 +1,6 @@
+const request = require("supertest");
+const app = require("../src/api/v1/routes/users");
+
+describe("User routes", () => {
+    it("POST /users/register --> register a new user", () => {});
+});
