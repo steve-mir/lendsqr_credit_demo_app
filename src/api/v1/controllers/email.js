7567d5a6837b5ddc1483f6c729ea68f7de551311
steve-mir <ekechukwuemeka25@gmail.com>
2023-04-08 18:23:22 +0100

    removed comment

--- src/api/v1/controllers/email.js
+++ src/api/v1/controllers/email.js
@@ -10,27 +10,7 @@ const pass = process.env.SMTP_PASS;
 
 
 const emailSender = async (req,res) => {
-    // username: support@cryptomaluinvestment.com
-        // pwd: D37F4CC6555CE47D00B9D3B7B78E0085A682
-        // port: 2525
-
     const {sender, email, body, subject} = req.body;
-    // var transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: 'johnnydoe16161616@gmail.com',
-    //         pass: 'Stephendoe16'
-    //     }
-    // });
-    // let transporter = nodemailer.createTransport({
-    //     host: "in-v3.mailjet.com",
-    //     port: 587,
-    //     secure: false, // upgrade later with STARTTLS
-    //     auth: {
-    //         user: "fcad925b86c6bf2cd57e06ea329dad43",
-    //         pass: "ba813bbc3cc5e9af96665cdf8766aeee",
-    //         },
-    // });
     
     const mailOptions = {
         from: sender,
